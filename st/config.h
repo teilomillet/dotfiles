3ac0504622ed4c7092421df44f919fa744a56f31
Teïlo <teilomillet@gmail.com>
2024-01-15 11:51:22 +0100

    add new dwm/st

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "LiberationMono:style=Bold:pixelsize=16:antialias=true:autohint=true";
-static int borderpx = 6;
+static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -94,52 +94,47 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.6;
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
-  /* 8 normal colors */
-  [0] = "#171a21", /* black   */
-  [1] = "#1f222c", /* red     */
-  [2] = "#2f3340", /* green   */
-  [3] = "#454a5c", /* yellow  */
-  [4] = "#60687f", /* blue    */
-  [5] = "#828ba6", /* magenta */
-  [6] = "#aeb4c5", /* cyan    */
-  [7] = "#e0e2e9", /* white   */
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
-  /* 8 bright colors */
-  [8]  = "#afbbde", /* black   */
-  [9]  = "#62ce79", /* red     */
-  [10] = "#ffedac", /* green   */
-  [11] = "#b9ac6a", /* yellow  */
-  [12] = "#76bbae", /* blue    */
-  [13] = "#8dbcbd", /* magenta */
-  [14] = "#d9d9d9", /* cyan    */
-  [15] = "#7c8dbb", /* white   */
+	[255] = 0,
 
-  /* special colors */
-  [256] = "#0e1015", /* background */
-  [257] = "#c5c9d5", /* foreground */
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 };
+
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
-
-/*
- * Colors used, when the specific fg == defaultfg. So in reverse mode this
- * will reverse too. Another logic would only make the simple feature too
- * complex.
- */
-static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;
-
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
